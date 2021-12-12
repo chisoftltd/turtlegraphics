870350f232ae1c8323cf12bf8d3778a7c2873719
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-12 22:29:55 +0000

    OOP

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-# This is a sample Python script.
-import another_module
-from turtle import Turtle
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi(another_module.another_variable)
+from turtle import Turtle, Screen
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-timmy = Turtle()
-print(timmy)
+blaze = Turtle()
+
+print(blaze)
+blaze.shape("turtle")
+blaze.color("green")
+
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
