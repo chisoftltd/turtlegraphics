e2dd4a784909267279618d97f6a9f350ca2c55ae
chisoftltd <chisoftltd@gmail.com>
2021-12-13 15:31:29 +0000

    Turtle and PrettyTable packages

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
+#
+# from turtle import Turtle, Screen
+#
+# blaze = Turtle()
+#
+# print(blaze)
+# blaze.shape("turtle")
+# blaze.color("green")
+#
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# blaze.position()
+# blaze.forward(95)
+# blaze.position()
+# blaze.right(45)
+# blaze.forward(-75)
+# blaze.lt(90)
+# blaze.forward(-75)
+# blaze.position()
+#
+# my_screen.exitonclick()
 
-from turtle import Turtle, Screen
+from prettytable import PrettyTable
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+x = PrettyTable()
+x.add_column("Names", ["Benjamin", "Joy", "Emmanuel", "Shepherd", "Mikael"])
+x.add_column("Gender", [49, 39, 9, 6, 3])
+x.align = 'l'
+print(x)
+print(x.get_string(start=1, end=3))
+print(x.get_string(sortby="Gender"))
+print(x.get_html_string(attributes={"id":"my_table", "class":"red_table"}))
 
-blaze = Turtle()
-
-print(blaze)
-blaze.shape("turtle")
-blaze.color("green")
-
-
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
