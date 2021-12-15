045e35d16fb80f1de913d6c961d50050e1d0ffc5
chisoftltd <chisoftltd@gmail.com>
2021-12-15 13:14:53 +0000

    Turtle and PrettyTable packages

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
 from turtle import Turtle, Screen
+import random
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
@@ -7,40 +7,20 @@ blaze_the_turtle = Turtle()
 
 print(blaze_the_turtle)
 blaze_the_turtle.resizemode("auto")
-
-for i in range(50):
-    blaze_the_turtle.shape("square")
-    blaze_the_turtle.pencolor("red")
-    blaze_the_turtle.forward(10 + i)
-    blaze_the_turtle.penup()
-    blaze_the_turtle.forward(20 + i)
-    blaze_the_turtle.shape("circle")
-    blaze_the_turtle.pencolor("blue")
-    blaze_the_turtle.pendown()
-    blaze_the_turtle.forward(30 + i)
-    blaze_the_turtle.penup()
-    # blaze_the_turtle.right(90)
-    blaze_the_turtle.shape("arrow")
-    blaze_the_turtle.pencolor("green")
-    blaze_the_turtle.pendown()
-    blaze_the_turtle.forward(30 + i)
-    blaze_the_turtle.right(90)
-    # blaze_the_turtle.forward(100 * i)
-    # blaze_the_turtle.right(90)
-    # blaze_the_turtle.forward(100 * i)
-    # blaze_the_turtle.right(90)
-    # blaze_the_turtle.forward(100 * i)
+colours = ["lightgreen", "DodgerBlue", "Navy", "Magenta", "Yellow", "SpringGreen", "Crimson",
+           "MediumVioletRed", "#0000FF", "Gold", "cyan", "deeppink"]
 
 
+def draw_shape(side_count):
+    angle = 360 / side_count
+    for _ in range(side_count):
+        blaze_the_turtle.forward(100)
+        blaze_the_turtle.right(angle)
 
 
-
-
-
-
-
-
-
+for i in range(3, 15):
+    blaze_the_turtle.color(random.choice(colours))
+    draw_shape(i)
 
 my_screen = Screen()
 print(my_screen.canvheight)
