17df0e661c8d2433a246c04ace6f96e4fe7ae03a
chisoftltd <chisoftltd@gmail.com>
2021-12-15 16:13:12 +0000

    Generate Random RGB Colours

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-from turtle import Turtle, Screen
+import turtle as t
 import random
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-blaze_the_turtle = Turtle()
+blaze_the_turtle = t.Turtle()
+t.colormode(255)
 
-print(blaze_the_turtle)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
 blaze_the_turtle.resizemode("auto")
-colours = ["lightgreen", "DodgerBlue", "Navy", "Magenta", "Yellow", "SpringGreen", "Crimson",
-           "MediumVioletRed", "#0000FF", "Gold", "cyan", "deeppink", "Blue", "PaleVioletRed"]
 directions = [0, 45, 90, 135, 180, 225, 270]
 
 for i in range(70):
     blaze_the_turtle.speed(i)
     blaze_the_turtle.pensize(i)
-    blaze_the_turtle.color(random.choice(colours))
-    blaze_the_turtle.forward(100)
-    blaze_the_turtle.right(random.choice(directions))
+    blaze_the_turtle.color(random_color())
+    blaze_the_turtle.forward(30)
+    blaze_the_turtle.setheading(random.choice(directions))
 
-
-my_screen = Screen()
+my_screen = t.Screen()
 print(my_screen.canvheight)
 my_screen.exitonclick()
