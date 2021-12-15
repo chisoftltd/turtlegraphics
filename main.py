f5777439b3e2de7c1d0385ab7a2f8431b692c2d8
chisoftltd <chisoftltd@gmail.com>
2021-12-15 15:12:09 +0000

    Random walks

--- main.py
+++ main.py
@@ -9,18 +9,15 @@ print(blaze_the_turtle)
 blaze_the_turtle.resizemode("auto")
 colours = ["lightgreen", "DodgerBlue", "Navy", "Magenta", "Yellow", "SpringGreen", "Crimson",
            "MediumVioletRed", "#0000FF", "Gold", "cyan", "deeppink", "Blue", "PaleVioletRed"]
+directions = [0, 45, 90, 135, 180, 225, 270]
 
-
-def draw_shape(side_count):
-    angle = 360 / side_count
-    for _ in range(side_count):
-        blaze_the_turtle.forward(100)
-        blaze_the_turtle.right(angle)
-
-
-for i in range(3, 15):
+for i in range(70):
+    blaze_the_turtle.speed(i)
+    blaze_the_turtle.pensize(i)
     blaze_the_turtle.color(random.choice(colours))
-    draw_shape(i)
+    blaze_the_turtle.forward(100)
+    blaze_the_turtle.right(random.choice(directions))
+
 
 my_screen = Screen()
 print(my_screen.canvheight)
