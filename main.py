1676bbcbd8ca51105616ba4a328dcf16d692e1a6
chisoftltd <chisoftltd@gmail.com>
2021-12-15 12:21:58 +0000

    Turtle and PrettyTable packages

--- main.py
+++ main.py
@@ -6,12 +6,24 @@ from turtle import Turtle, Screen
 blaze_the_turtle = Turtle()
 
 print(blaze_the_turtle)
-blaze_the_turtle.shape("arrow")
-blaze_the_turtle.color("blue")
 blaze_the_turtle.resizemode("auto")
 
-for _ in range(4):
-    blaze_the_turtle.forward(100)
+for i in range(50):
+    blaze_the_turtle.shape("square")
+    blaze_the_turtle.pencolor("red")
+    blaze_the_turtle.forward(10 + i)
+    blaze_the_turtle.penup()
+    blaze_the_turtle.forward(20 + i)
+    blaze_the_turtle.shape("circle")
+    blaze_the_turtle.pencolor("blue")
+    blaze_the_turtle.pendown()
+    blaze_the_turtle.forward(30 + i)
+    blaze_the_turtle.penup()
+    # blaze_the_turtle.right(90)
+    blaze_the_turtle.shape("arrow")
+    blaze_the_turtle.pencolor("green")
+    blaze_the_turtle.pendown()
+    blaze_the_turtle.forward(30 + i)
     blaze_the_turtle.right(90)
     # blaze_the_turtle.forward(100 * i)
     # blaze_the_turtle.right(90)
